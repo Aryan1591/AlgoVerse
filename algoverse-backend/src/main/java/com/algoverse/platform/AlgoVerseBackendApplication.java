a6e802e72b9ba0601a10db4280f0bea258facb2a
Avishekh1 <avishekh.thakur@zemosolabs.com>
2025-11-28 01:23:14 +0530

    Initial Commit

---
+++ algoverse-backend/src/main/java/com/algoverse/platform/AlgoVerseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.algoverse.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoVerseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoVerseBackendApplication.class, args);
+	}
+
+}
---
+++ algoverse-backend/src/test/java/com/algoverse/platform/AlgoVerseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algoverse.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoVerseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
